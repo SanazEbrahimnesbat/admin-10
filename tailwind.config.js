0a5396ce5d6bf6d1703276201f6b4ec3c5d74564
houssein ashrafipoor <ashrafipoor@gmail.com>
2022-03-07 19:06:06 +0100

    config tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#283144",
+          "secondary": "#e879f9",
+          "accent": "#00CFDD",
+          "neutral": "#A1B0CB",
+          "base-100": "#1C222F",
+          "info": "#5A8DEE",
+          "success": "#35BE7C",
+          "warning": "#DF9C45",
+          "error": "#FF5B5C",
+        },
+      },
+    ],
+  },
+}
